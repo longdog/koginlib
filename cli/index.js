15b1f2f53184a8452dcf32c22e9d256bf7dc59c9
longdog <linda@longdog.us>
2021-07-15 14:28:03 +0400

    refactor cli

--- cli/index.js
+++ cli/index.js
@@ -9,20 +9,26 @@ function getImage(canvas, filepath) {
   out.on("finish", () => console.log("The PNG file was created."));
 }
 
+function getPatternFile(filepath) {
+  const patternFile = readFileSync(filepath);
+  return patternFile.toString();
+}
+
 const DOT = 20;
 const STITCH_LINE = 5;
 const [CANVAS_WIDTH, CANVAS_HEIGHT] = [29 * DOT + DOT / 2, 280.5];
 
-const pattern = patternNodeFactory(
-  {
-    width: CANVAS_WIDTH * 2,
-    height: CANVAS_HEIGHT * 2 - DOT,
-  },
-  {
-    stitchStep: DOT,
-    stitchWeight: STITCH_LINE,
-  }
-);
+const canvasSize = {
+  width: CANVAS_WIDTH * 2,
+  height: CANVAS_HEIGHT * 2 - DOT,
+};
+
+const stitch = {
+  stitchStep: DOT,
+  stitchWeight: STITCH_LINE,
+};
+
+const pattern = patternNodeFactory(canvasSize, stitch);
 
 const argArr = process.argv;
 
@@ -31,15 +37,14 @@ const args = argArr.reduce((obj, el) => {
   obj[kv[0]] = kv[1] ?? "";
   return obj;
 }, {});
-let canvas = undefined;
+let p = undefined;
 const withGrid = !args.hasOwnProperty("--nogrid");
 if (args["--pattern"]) {
-  const patternFile = readFileSync(args["--pattern"]);
-  canvas = pattern(patternFile.toString(), withGrid).canvas;
+  p = pattern(getPatternFile(args["--pattern"]), withGrid);
 } else {
   const g = patternGenerator(pattern, withGrid)(true);
-  canvas = g.next().value.canvas;
+  p = g.next().value;
 }
 
 const path = args["--output"] ?? __dirname + "/pattern.png";
-getImage(canvas, path);
+getImage(p.canvas, path);
